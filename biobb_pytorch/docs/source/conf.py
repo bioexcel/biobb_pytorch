8894f3e13d622e57c979d8a83e683b2422d5f015
gbayarri <genis.bayarri@irbbarcelona.org>
2025-01-08 16:35:18 +0100

    Year updated

--- biobb_pytorch/docs/source/conf.py
+++ biobb_pytorch/docs/source/conf.py
@@ -69,7 +69,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "biobb_pytorch"
-copyright = "2024, Bioexcel Project"
+copyright = "2025, Bioexcel Project"
 author = "Bioexcel Project"
 
 # The version info for the project you're documenting, acts as replacement for

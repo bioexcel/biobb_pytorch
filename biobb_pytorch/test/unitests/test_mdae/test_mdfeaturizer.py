5390f65adec530d82d36027dd1f6dc9f53bf812a
PieterZanders <pieterzanders@gmail.com>
2026-01-28 17:43:09 +0100

    fix mdfeature test

--- biobb_pytorch/test/unitests/test_mdae/test_mdfeaturizer.py
+++ biobb_pytorch/test/unitests/test_mdae/test_mdfeaturizer.py
@@ -1,18 +1,18 @@
 # type: ignore
 from biobb_common.tools import test_fixtures as fx
-from biobb_pytorch.mdae.mdfeaturizer import mdfeaturizer
+from biobb_pytorch.mdae.mdfeaturizer import MDFeaturizer
 import torch
 
 
 class TestMDFeaturizer:
     def setup_class(self):
-        fx.test_setup(self, 'mdfeaturizer')
+        fx.test_setup(self, 'MDFeaturizer')
 
     def teardown_class(self):
         fx.test_teardown(self)
 
-    def test_mdfeaturizer(self):
-        mdfeaturizer(properties=self.properties, **self.paths)
+    def test_MDFeaturizer(self):
+        MDFeaturizer(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_dataset_pt_path'])
         assert fx.not_empty(self.paths['output_stats_pt_path'])
 
